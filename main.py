b3b29faf42f8241308990dab5848aff7dbf2c61f
Alexrazumovich <alexrazumovich@gmail.com>
2024-10-17 16:25:58 +0300

    CreateForm  in Python

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATION']=False
+
+db=SQLAlchemy(app)
+
+class User(db.Model):
+   id=db.Column(db.Integer, primary_key=True)
+   username=db.Column(db.String(80),unique=True, nullable=False)
+   def __repr__(self):
+        return f'<User {self.username}>'
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/add_user')
+def add_user():
+    new_user=User(username='JohnDoe')
+    db.session.add(new_user)
+    db.session.commit()
+    return 'User added successfully!'
+@app.route('/users')
+def get_users():
+    users=User.query.all()
+    return str([user.username for user in users])
+if __name__ == '__main__':
+    app.run(debug=True)
